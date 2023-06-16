16bb6f52b52dac6c6760d35dccb66e584cd05bfb
maazjnr <goalmaaz@gmail.com>
2023-06-16 01:12:33 +0100

    add

--- client/src/App.js
+++ client/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Mern Recipe
+          Pyservant & Maazscript
         </a>
       </header>
     </div>
